ba599be34f57a4060656a33e65afed46ef070fa8
Mohammad AbuShady <coalwater5@gmail.com>
2015-02-27 23:22:39 +0200

    Use class name for method name generation

--- lib/acts_as_interval/acts_as_interval.rb
+++ lib/acts_as_interval/acts_as_interval.rb
@@ -16,14 +16,19 @@ module ActsAsInterval
     end
 
     module LocalInstanceMethods
-      def intervals_before
-        self.class.where('ends_at <= ?', self.send(start_field))
-      end
-      def intervals_after
-        self.class.where('starts_at >= ?', self.send(end_field))
-      end
-      def overlapping_intervals
-        self.class.where("DATEDIFF(#{start_field}, :my_end) * DATEDIFF(:my_start, #{end_field}) >= 0", my_start: self.send(start_field), my_end: self.send(end_field)).where.not(id: self.id)
+      def self.included(klass)
+        klass_name = klass.name.underscore.pluralize
+        define_method "past_#{klass_name}" do
+          self.class.where('ends_at <= ?', self.send(start_field))
+        end
+        define_method "future_#{klass_name}" do
+          self.class.where('starts_at >= ?', self.send(end_field))
+        end
+        define_method "overlapping_#{klass_name}" do
+          self.class.where("DATEDIFF(#{start_field}, :my_end) * DATEDIFF(:my_start, #{end_field}) >= 0", my_start: self.send(start_field), my_end: self.send(end_field)).where.not(id: self.id)
+        end
+        alias_method :intervals_before, "past_#{klass_name}"
+        alias_method :intervals_after, "future_#{klass_name}"
       end
     end
   end
--- test/acts_as_interval_test.rb
+++ test/acts_as_interval_test.rb
@@ -1,13 +1,25 @@
 require 'test_helper'
 
 class ActsAsIntervalTest < ActiveSupport::TestCase
+  def test_future_interval_method
+    interval = Interval.new
+    assert_equal true, interval.respond_to?(:future_intervals)
+  end
+  def test_past_interval_method
+    interval = Interval.new
+    assert_equal true, interval.respond_to?(:past_intervals)
+  end
+  def test_overlapping_interval_method
+    interval = Interval.new
+    assert_equal true, interval.respond_to?(:overlapping_intervals)
+  end
   def test_intervals_before
     old_interval = Interval.create(starts_at: 1.month.ago, ends_at: 1.week.ago)
     older_interval = Interval.create(starts_at: 2.year.ago, ends_at: 1.year.ago)
     Interval.create(starts_at: 1.month.since, ends_at: 1.year.since)
     Interval.create(starts_at: 1.years.ago, ends_at: 1.years.since)
     new_interval = Interval.create(starts_at: 1.day.ago, ends_at: 1.hour.ago)
-    assert_equal new_interval.intervals_before, [old_interval, older_interval]
+    assert_equal new_interval.past_intervals, [old_interval, older_interval]
   end
   def test_intervals_after
     Interval.create(starts_at: 1.month.ago, ends_at: 1.week.ago)
@@ -15,13 +27,6 @@ class ActsAsIntervalTest < ActiveSupport::TestCase
     after_interval = Interval.create(starts_at: 1.month.since, ends_at: 1.year.since)
     Interval.create(starts_at: 1.years.ago, ends_at: 1.years.since)
     new_interval = Interval.create(starts_at: 1.day.ago, ends_at: 1.hour.ago)
-    assert_equal new_interval.intervals_after, [after_interval]
-  end
-  def test_intersecting_intervals
-    my_interval = Interval.create(starts_at: 1.days.ago, ends_at: 1.days.since)
-    Interval.create(starts_at: 1.year.ago, ends_at: 1.month.ago)
-    Interval.create(starts_at: 1.year.since, ends_at: 2.years.since)
-    intersecting_interval = Interval.create(starts_at: 1.month.ago, ends_at: 1.month.since)
-    assert_equal my_interval.overlapping_intervals, [intersecting_interval]
+    assert_equal new_interval.future_intervals, [after_interval]
   end
 end
